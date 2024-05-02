2dd4e6f29387f88a1dd131839b7f393ef665cbf9
André Rodrigues Correia <andrecorreiati@gmail.com>
2024-05-02 12:36:51 -0300

    adjust

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const PORT = 21185
 // Servir arquivos estáticos da pasta 'dist'
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(history({
-  rewrites: [
-    { from: /\/signUp/, to: '/index.html' }, // Redireciona /signUp para o index.html
-    // Adicione outras regras de reescrita conforme necessário
-  ]
+  disableDotRule: true,
+  verbose: true
 }));
 
 // Rota para servir o index.html
