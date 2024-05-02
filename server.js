9ebb60a5613d2982b61e9e874b0799ea3c472b7e
André Rodrigues Correia <andrecorreiati@gmail.com>
2024-05-02 12:48:34 -0300

    adjust

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const path = require('path');
-const history = require('connect-history-api-fallback')
 
 const app = express();
 const PORT = 21185
 
 // Servir arquivos estáticos da pasta 'dist'
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use(history({
-  rewrites: [
-    { from: /\/signUp/, to: '/index.html' }, // Redireciona /signUp para o index.html
-    // Adicione outras regras de reescrita conforme necessário
-  ]
-}));
+
 
 // Rota para servir o index.html
 app.get('/', (req, res) => {
