8609a27d22c2440a610948efc51c024ef4f76cbc
André Rodrigues Correia <andrecorreiati@gmail.com>
2024-05-02 11:23:02 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 21185
+
+// Servir arquivos estáticos da pasta 'dist'
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Rota para servir o index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+// Iniciar o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
