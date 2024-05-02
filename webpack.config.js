063902294734832bf383075d61979949f1386af4
André Rodrigues Correia <andrecorreiati@gmail.com>
2024-05-02 12:44:24 -0300

    adjust

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     //...
     devServer: {
+      historyApiFallback: true,
       historyApiFallback: {
         rewrites: [
           { from: /^\/signUp/, to: '/views/SignUpView.html' },
