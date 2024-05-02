fa4113d2f508eecf2ffc962fce22c696d3ba8729
André Rodrigues Correia <andrecorreiati@gmail.com>
2024-05-02 16:16:44 -0300

    adjust

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,8 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     rewrites: [
-      // { from: /^\/$/, to: '/views/landing.html' },
-      // { from: /^\/subpage/, to: '/views/subpage.html' },
-      // { from: /./, to: '/views/404.html' },
+      { from: /^\/$/, to: '/views/index.html' },
+      { from: /^\/signUp/, to: '/views/signUp.html' },
     ],
   },
 };
