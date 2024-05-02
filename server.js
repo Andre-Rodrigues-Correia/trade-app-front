92922584b55a879e0fedc83f5f70906d8da313b1
André Rodrigues Correia <andrecorreiati@gmail.com>
2024-05-02 15:31:09 -0300

    adjust

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const path = require('path');
+const helmet = require('helmet');
 
 const app = express();
-const PORT = 21185
+const PORT = 443
 
 // Servir arquivos estáticos da pasta 'dist'
 app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use((req, res, next) => {
+  if (req.secure) {
+    // Solicitação HTTPS
+    next();
+  } else {
+    // Redirecionar para HTTPS
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+});
+
+app.use(helmet.hsts({
+  maxAge: 31536000, // 1 ano em segundos
+  includeSubDomains: true,
+  preload: true
+}));
+
 
 
 // Rota para servir o index.html
