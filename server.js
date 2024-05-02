856c91eac498ed464cfd729ddf195b192ea1e770
André Rodrigues Correia <andrecorreiati@gmail.com>
2024-05-02 15:45:35 -0300

    adjust

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ app.use((req, res, next) => {
     next();
   } else {
     // Redirecionar para HTTPS
-    console.log(req.url)
-    res.redirect('https://' + req.headers.host + req.url);
+    res.redirect('andrerodriguescorreia.com.br:443');
   }
 });
 
